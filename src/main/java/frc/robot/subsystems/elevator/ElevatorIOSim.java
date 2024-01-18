d937d33cca8dfdeb739fdc9cc5cdedef5841566f
AVA HUTCHISON <HUTCHISONAVA14@gmail.com>
2024-01-17 20:02:08 -0600

    ElevatorIO Implements 20:02 1/17/24

---
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorIOSim.java
@@ -0,0 +1,29 @@
+package frc.robot.subsystems.elevator;
+
+public class ElevatorIOSim implements ElevatorIO{
+
+    @Override
+    public void updateInputs() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateInputs'");
+    }
+
+    @Override
+    public void setVoltage(double volts) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setVoltage'");
+    }
+
+    @Override
+    public double getCurrentVelocity() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getCurrentVelocity'");
+    }
+
+    @Override
+    public double getCurrentHeight() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getCurrentHeight'");
+    }
+    
+}
