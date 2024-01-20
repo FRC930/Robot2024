22c8910103c28cb8ea9e4e8052e2bbf8eed6bd02
JackLaFreniere <97800485+JackLaFreniere@users.noreply.github.com>
2024-01-20 14:14:46 -0600

    1/20/24 2:14 Fixed spacing in robot and main

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -31,7 +31,7 @@ public class Robot extends LoggedRobot {
 
   private RobotContainer m_robotContainer;
 
-/**
+  /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
@@ -91,7 +91,7 @@ public class Robot extends LoggedRobot {
     m_robotContainer = new RobotContainer();
   }
 
-/**
+  /**
    * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
    * that you want ran during disabled, autonomous, teleoperated and test.
    *
@@ -100,14 +100,14 @@ public class Robot extends LoggedRobot {
    */
   @Override
   public void robotPeriodic() {
-// Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
     // commands, running already-scheduled commands, removing finished or interrupted commands,
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run(); 
   }
 
-/** This function is called once each time the robot enters Disabled mode. */
+  /** This function is called once each time the robot enters Disabled mode. */
   @Override
   public void disabledInit() {}
 
@@ -122,13 +122,13 @@ public class Robot extends LoggedRobot {
   public void autonomousInit() {
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
-// schedule the autonomous command (example)
+  // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
       m_autonomousCommand.schedule();
     }
   }
 
-/** This function is called periodically during autonomous. */
+  /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {}
 
@@ -137,7 +137,7 @@ public class Robot extends LoggedRobot {
 
   @Override
   public void teleopInit() {
-// This makes sure that the autonomous stops running when
+    // This makes sure that the autonomous stops running when
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
     // this line or comment it out.
@@ -146,7 +146,7 @@ public class Robot extends LoggedRobot {
     }
   }
 
-/** This function is called periodically during operator control. */
+  /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {}
 
@@ -155,15 +155,15 @@ public class Robot extends LoggedRobot {
 
   @Override
   public void testInit() {
-// Cancels all running commands at the start of test mode.
+    // Cancels all running commands at the start of test mode.
     CommandScheduler.getInstance().cancelAll();
   }
 
-/** This function is called periodically during test mode. */
+  /** This function is called periodically during test mode. */
   @Override
   public void testPeriodic() {}
 
-/** This function is called once when the robot is first started up. */
+  /** This function is called once when the robot is first started up. */
   @Override
   public void simulationInit() {}
 
