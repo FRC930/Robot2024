7ffa85b509923f6bdc177c487c9e43e664b5231d
Jack Schumacher <84467112+jackrschumacher@users.noreply.github.com>
2024-02-06 18:04:32 -0600

    (18:03 2/6/23) Update Comments and adjust code in ElevatorType for readability

--- src/main/java/frc/robot/subsystems/elevator/ElevatorType.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorType.java
@@ -1,9 +1,9 @@
 package frc.robot.subsystems.elevator;
 
 public enum ElevatorType {
-    //1 divided by diameter * Pi * gear ratio of 2. TODO test to see if closer to an inch   
-    //1 divided by diameter * Pi * (1/gear ratio of 2). Pretty close to an inch
-    SHOOTING_ELEVATOR(1 / (1.253 * Math.PI * 0.5),1,1,0,12,0), //TODO: Configure constants
+    //1 divided by diameter * Pi * gear ratio <2>. TODO test to see if closer to an inch   
+    //1 divided by diameter * Pi * (1/gear ratio <2>). Pretty close to an inch
+    SHOOTING_ELEVATOR(1 / (1.253 * Math.PI * (1/2)),1,1,0,12,0), //TODO: Configure constants
     CLIMBING_ELEVATOR(1,1,1,0,5,0); //TODO: Configure constants
 
     public final double m_gearRatio;
