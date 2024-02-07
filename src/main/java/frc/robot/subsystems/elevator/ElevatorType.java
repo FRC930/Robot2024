25e8971b2addcb38c1a5f44b65fcc5a0c7ac26b6
Jack Schumacher <84467112+jackrschumacher@users.noreply.github.com>
2024-02-06 20:23:14 -0600

    (20:22 2/6) Update ElevatorType to change gear ratio values to doubles

--- src/main/java/frc/robot/subsystems/elevator/ElevatorType.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorType.java
@@ -3,7 +3,7 @@ package frc.robot.subsystems.elevator;
 public enum ElevatorType {
     //1 divided by diameter * Pi * gear ratio <2>. TODO test to see if closer to an inch   
     //1 divided by diameter * Pi * (1/gear ratio <2>). Pretty close to an inch
-    SHOOTING_ELEVATOR(1 / (1.253 * Math.PI * (1/2)),1,1,0,12,0), //TODO: Configure constants
+    SHOOTING_ELEVATOR(1 / (1.253 * Math.PI * (1.0/2.0)),1,1,0,12,0), //TODO: Configure constants
     CLIMBING_ELEVATOR(1,1,1,0,5,0); //TODO: Configure constants
 
     public final double m_gearRatio;
