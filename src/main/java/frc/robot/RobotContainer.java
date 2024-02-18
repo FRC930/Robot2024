f93c7692b52301e0d7801c30a4e501b991739a1a
AVA HUTCHISON <HUTCHISONAVA14@gmail.com>
2024-02-17 19:56:17 -0600

    Commented Out ConfigureCoDriverBindings for Sussex 2/17/24 19:56

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -291,7 +291,7 @@ public class RobotContainer {
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the trigger bindings
-    configureCoDriverBindingsForTesting();
+    // configureCoDriverBindingsForTesting();
     configureDriverBindings();
     portForwardCameras();
     // set our own visionMeasurementDeviations
