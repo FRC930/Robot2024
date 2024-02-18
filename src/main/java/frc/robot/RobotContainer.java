2fa5f78ea4d1edafa72ae8a556f3c615032985a3
Harry <96154793+WasteOfTheOcean@users.noreply.github.com>
2024-02-18 08:57:30 -0600

    Increased Angular Max Velocity to 1 Rotation per Second 2/18/24 8:57

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -120,7 +120,7 @@ public class RobotContainer {
 
     //Use max speed from tuner constants from webpage
     final double MaxSpeed = TunerConstants.kMaxSpeed;
-    final double MaxAngularRate = 1.5 * Math.PI; // 3/4 a rotation per second max angular velocity  
+    final double MaxAngularRate = 2.0 * Math.PI; // 1 rotation per second max angular velocity  
 
     /* Setting up bindings for necessary control of the swerve drive platform */
     SwerveDrivetrainSubsystem drivetrain = TunerConstants.DriveTrain; // My drivetrain
