3ab5d93a4e4b00bf40c3f700da398afb603e1575
Keegan Rubingh <keeganrubingh@gmail.com>
2024-02-22 20:31:41 -0600

    Corrected shooter speed constants 2/22/24 - 20:31

--- src/main/java/frc/robot/utilities/SpeakerScoreUtility.java
+++ src/main/java/frc/robot/utilities/SpeakerScoreUtility.java
@@ -23,7 +23,7 @@ public class SpeakerScoreUtility {
     static final private int PIVOT_ANGLE_SPEED_COLUMN = 3;
     static final private int EVELATOR_HEIGHT_COLUMN = 4;   // TODO not need given all at 0.0 
 
-    static final private double COMPUTED_SHOOT_SPEED = 170.0;
+    static final private double COMPUTED_SHOOT_SPEED = 140.0;
 
     static final private int CLOSE_ROW = 0;
     static final private int MEDIUM_ROW = 1;
@@ -32,7 +32,7 @@ public class SpeakerScoreUtility {
     {
         {140.0, 140.0, 0.9, 40.0, 0.0}, // "close" 3' 7" from speaker bumper to front of frame
         {140.0, 140.0, 0.9, 33.0, 0.0}, // "medium" 7' 8" from speaker bumper to front of frame
-        {170.0, 170.0, 0.9, 31.0, 0.0} // "far" 12' 7" from speaker bumper to front of frame (ONLY WORKS WITH UNTRIMMED NOTES)
+        {140.0, 140.0, 0.9, 31.0, 0.0} // "far" 12' 7" from speaker bumper to front of frame (ONLY WORKS WITH UNTRIMMED NOTES)
     };
 
 
