2a4200cd6ab25bc1ef0b6eaec7b683688efb8db3
Harry <96154793+WasteOfTheOcean@users.noreply.github.com>
2024-02-28 12:24:22 -0600

    Raised reset value of pivot to 1.67 degrees because thats the angle its booting as

--- src/main/java/frc/robot/subsystems/pivot/PivotSubsystem.java
+++ src/main/java/frc/robot/subsystems/pivot/PivotSubsystem.java
@@ -7,6 +7,7 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.filter.Debouncer;
 import edu.wpi.first.math.filter.Debouncer.DebounceType;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -72,7 +73,7 @@ public class PivotSubsystem extends SubsystemBase{
             Logger.recordOutput(this.getClass().getSimpleName() + "/LimitSwitchRangeWhenReset", m_sensorIO.getRange());
             TalonFX m_motor = ((PivotIORobot) m_io).m_motor;    
             Phoenix6Utility.applyConfigAndNoRetry(m_motor,
-                () -> m_motor.getConfigurator().setPosition(0.0));
+                () -> m_motor.getConfigurator().setPosition(Units.degreesToRotations(1.67))); // It boots as 1.67 instead of 0.0
         }
     }
 
