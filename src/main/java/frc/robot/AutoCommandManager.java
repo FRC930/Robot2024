1170a1e61df3fe9e037bd5276ca4c533526d395d
Keegan Rubingh <keeganrubingh@gmail.com>
2024-03-25 18:16:23 -0500

    Added parallel race groups to gamepiece intake commands. 3/25/24 - 18:16

--- src/main/java/frc/robot/AutoCommandManager.java
+++ src/main/java/frc/robot/AutoCommandManager.java
@@ -105,21 +105,29 @@ public class AutoCommandManager {
         PivotSubsystem pivot) { 
         //TODO update all of the x and y positions for each of the alliance colors (don't do center line points)
         NamedCommands.registerCommand("AllianceTopNote", new LimeLightIntakeCommand(drivetrain, gamePieceUtility, 
-            new Pose2d(2.9, 7.0, new Rotation2d(0.0)), new Pose2d(13.68, 7.0, new Rotation2d(0.0))));
+            new Pose2d(2.9, 7.0, new Rotation2d(0.0)), new Pose2d(13.68, 7.0, new Rotation2d(0.0)))
+            .raceWith(indexer.newUntilNoteFoundCommand().raceWith(new WaitCommand(1.0))));
         NamedCommands.registerCommand("AllianceMidNote", new LimeLightIntakeCommand(drivetrain, gamePieceUtility, 
-            new Pose2d(2.9, 5.55, new Rotation2d(0.0)), new Pose2d(13.68, 5.55, new Rotation2d(0.0))));
+            new Pose2d(2.9, 5.55, new Rotation2d(0.0)), new Pose2d(13.68, 5.55, new Rotation2d(0.0)))
+            .raceWith(indexer.newUntilNoteFoundCommand().raceWith(new WaitCommand(1.0))));
         NamedCommands.registerCommand("AllianceLowNote", new LimeLightIntakeCommand(drivetrain, gamePieceUtility, 
-            new Pose2d(2.9, 4.1, new Rotation2d(0.0)), new Pose2d(13.68, 4.1, new Rotation2d(0.0))));
+            new Pose2d(2.9, 4.1, new Rotation2d(0.0)), new Pose2d(13.68, 4.1, new Rotation2d(0.0)))
+            .raceWith(indexer.newUntilNoteFoundCommand().raceWith(new WaitCommand(1.0))));
         NamedCommands.registerCommand("MidLineLevel1", new LimeLightIntakeCommand(drivetrain, gamePieceUtility, 
-            new Pose2d(8.29, 0.77, new Rotation2d(0.0))));
+            new Pose2d(8.29, 0.77, new Rotation2d(0.0)))
+            .raceWith(indexer.newUntilNoteFoundCommand().raceWith(new WaitCommand(1.0))));
         NamedCommands.registerCommand("MidLineLevel2", new LimeLightIntakeCommand(drivetrain, gamePieceUtility, 
-            new Pose2d(8.29, 2.44, new Rotation2d(0.0))));
+            new Pose2d(8.29, 2.44, new Rotation2d(0.0)))
+            .raceWith(indexer.newUntilNoteFoundCommand().raceWith(new WaitCommand(1.0))));
         NamedCommands.registerCommand("MidLineLevel3", new LimeLightIntakeCommand(drivetrain, gamePieceUtility, 
-            new Pose2d(8.29, 4.1, new Rotation2d(0.0))));
+            new Pose2d(8.29, 4.1, new Rotation2d(0.0)))
+            .raceWith(indexer.newUntilNoteFoundCommand().raceWith(new WaitCommand(1.0))));
         NamedCommands.registerCommand("MidLineLevel4", new LimeLightIntakeCommand(drivetrain, gamePieceUtility, 
-            new Pose2d(8.29, 5.78, new Rotation2d(0.0))));
+            new Pose2d(8.29, 5.78, new Rotation2d(0.0)))
+            .raceWith(indexer.newUntilNoteFoundCommand().raceWith(new WaitCommand(1.0))));
         NamedCommands.registerCommand("MidLineLevel5", new LimeLightIntakeCommand(drivetrain, gamePieceUtility, 
-            new Pose2d(8.29, 7.44, new Rotation2d(0.0))));
+            new Pose2d(8.29, 7.44, new Rotation2d(0.0)))
+            .raceWith(indexer.newUntilNoteFoundCommand().raceWith(new WaitCommand(1.0))));
 
         NamedCommands.registerCommand("setFar", speakerUtil.setDesiredTargetCommand(Target.far));
         NamedCommands.registerCommand("setMid", speakerUtil.setDesiredTargetCommand(Target.medium));
