fd59b6be9ec243bfc49703aa45c1903814f2ec25
Jack Schumacher <84467112+jackrschumacher@users.noreply.github.com>
2024-03-30 11:25:37 -0500

    based on Q76 greg stated we were shooting to the right/source side increased y value on target by 6 inches

--- src/main/java/frc/robot/commands/TurretAimCommand.java
+++ src/main/java/frc/robot/commands/TurretAimCommand.java
@@ -56,7 +56,7 @@ public class TurretAimCommand extends Command{
         m_AmpSideRedTargetPose = m_AprilTagFieldLayout.getTagPose(4).get().toPose2d();
         m_AmpSideRedTargetPose = new Pose2d(
             m_AmpSideRedTargetPose.getX() + 0.5,
-            m_AmpSideRedTargetPose.getY() - Units.inchesToMeters(23.0 + 12.0), 
+            m_AmpSideRedTargetPose.getY() - Units.inchesToMeters(23.0 + 12.0 - 6.0), 
             m_AmpSideRedTargetPose.getRotation());
         m_AmpSideBlueTargetPose = m_AprilTagFieldLayout.getTagPose(7).get().toPose2d();
         m_AmpSideBlueTargetPose = new Pose2d(
