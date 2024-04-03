11778b5e1a87c6ba75d43506b3986d6b6fce1ff3
Keegan Rubingh <keeganrubingh@gmail.com>
2024-04-02 19:00:54 -0500

    Commented named commands 4/2/24 - 19:00

--- src/main/java/frc/robot/AutoCommandManager.java
+++ src/main/java/frc/robot/AutoCommandManager.java
@@ -107,6 +107,22 @@ public class AutoCommandManager {
         SpeakerScoreUtility speakerUtil, 
         IntakeSubsystem intake, 
         PivotSubsystem pivot) { 
+
+        /*
+         * - - Gamepiece Intake Commands - -
+         * These commands attempt to intake a note at a given position. Assuming the game-piece camera can see the note, it will drive to it and intake it.
+         * NOTE - These do NOT start the intake, they just tell the limelight to drive to a detected note.
+         * 
+         * Field layout:
+         * (amp side)
+         *  Alliance  MidLine
+         *  |  top   |  Level 5
+            |  mid   |  Level 4
+         *  |  low   |  Level 3
+         *  |        |  Level 2
+         *  |        |  Level 1
+         * (source side)
+         */
         //TODO update all of the x and y positions for each of the alliance colors (don't do center line points)
         NamedCommands.registerCommand("AllianceTopNote", new LimeLightIntakeCommand(drivetrain, gamePieceUtility, 
             new Pose2d(2.9, 7.0, new Rotation2d(0.0)), new Pose2d(13.68, 7.0, new Rotation2d(0.0))));
@@ -135,47 +151,80 @@ public class AutoCommandManager {
         // Waits until there is a note in the indexer or for 1 second. (So we don't move beyond the midline)
         NamedCommands.registerCommand("waitUntilNote", indexer.newUntilNoteFoundCommand().raceWith(new WaitCommand(1.0)));
 
-
+        //Sets the shooter to a shooter preset
         NamedCommands.registerCommand("setFar", speakerUtil.setDesiredTargetCommand(Target.far));
         NamedCommands.registerCommand("setMid", speakerUtil.setDesiredTargetCommand(Target.medium));
         NamedCommands.registerCommand("setClose", speakerUtil.setDesiredTargetCommand(Target.close));
+
+        //Prepares, shoots, and stops the shooter, pivot, and turret
+        //NOTE - Can only be used when stationary
         NamedCommands.registerCommand("aimAndShoot",
             CommandFactoryUtility.createTurretPreaimCommand(turret)
                 .andThen(CommandFactoryUtility.createSpeakerScoreCommand(speakerUtil, shooter, pivot, indexer, turret))
                 .andThen(CommandFactoryUtility.createStopShootingCommand(shooter, indexer, pivot, turret))
         );
+
+        //Shoots and stops the shooter, pivot, indexer, and turret
         NamedCommands.registerCommand("shoot",
             CommandFactoryUtility.createSpeakerScoreCommand(speakerUtil, shooter, pivot, indexer, turret)
                 .andThen(CommandFactoryUtility.createStopShootingCommand(shooter, indexer, pivot, turret))
         );
+
+        //Shoots the note currently in the shooter, does not stow.
         NamedCommands.registerCommand("shootNoStow", CommandFactoryUtility.createSpeakerScoreCommand(speakerUtil, shooter, pivot, indexer, turret));
+        
+        //Aims the turret instantaneously
         NamedCommands.registerCommand("aim", new TurretAimCommand(turret));
+
+        //Turns on the intake without doing anything with the indexer
         NamedCommands.registerCommand("intakeNoIndexer", 
             CommandFactoryUtility.createIntakeNoIndexerCommand(intake));
+        
+        //Runs the intake and indexer to intake a note. Stops the intake once we get a note.
         NamedCommands.registerCommand("intake", 
             CommandFactoryUtility.createRunIntakeCommand(intake, indexer, turret));
+        
+        //Scores into the amp.
         NamedCommands.registerCommand("ampScore", 
             new PrintCommand("TODO add new amp command.") //TODO implement new amp here
-        );    
+        );  
+        
+        //Stops the intake and does a note backup
         NamedCommands.registerCommand("stopIntake", CommandFactoryUtility.createNoteBackUpCommand(indexer, intake, true));
+
+        //TODO: Ask harry what exactly it does. I know about as much about it as the name indicates.
         NamedCommands.registerCommand("movingSideShoot", 
             CommandFactoryUtility.createPrepareShootCommand(turret, pivot, shooter, 39.0)
                 .andThen(CommandFactoryUtility.createShootPreparedCommand(indexer))
                 .andThen(CommandFactoryUtility.createStopShootingCommand(shooter, indexer, turret))
                 .andThen(pivot.newSetPosCommand(35.0)) // pivot angle for pillar shot
         );
+        
+        // Turns the turret and pivot towards the speaker and spins up the shooter.
         NamedCommands.registerCommand("prepareShoot", CommandFactoryUtility.createPrepareShootCommand(turret, pivot, shooter, null));
+
+        // The same as prepareShoot except we keep turning the pivot.
         NamedCommands.registerCommand("prepareShootEndless", CommandFactoryUtility.createPrepareShootEndlessCommand(turret, pivot, shooter, null));
+
+        // Shoots from the shooting position used in AmpY
         // Used in AmpY3 & AmpY5
         NamedCommands.registerCommand("prepareAmpYShoot", CommandFactoryUtility.createPreparePosedShootEndlessCommand(turret, pivot, shooter, null, 
                 new Pose2d(11.2, 6.89, new Rotation2d(0.0)), 
                 // y: 6.89 original value
                 // moved since it was too far to the left 
                 new Pose2d(5.38, 5.89, new Rotation2d(0.0)) ));
+        
+        // Shoots assuming the shooter is already spun up and prepared to shoot. Does not stop the shooter.
         NamedCommands.registerCommand("preparedShoot", CommandFactoryUtility.createShootPreparedCommand(indexer));
+
+        // Stops the shooter and stows the indexer and pivot
         NamedCommands.registerCommand("stopShoot", CommandFactoryUtility.createStopShootingCommand(shooter, indexer, pivot, turret));
+
+        // TODO: Ask harry what exactly it does. I know about as much about it as the name indicates.
         NamedCommands.registerCommand("prepareNonAmpYShoot3or4", CommandFactoryUtility.createPrepareShootCommand(turret, pivot, shooter, 
                 25.5));
+        
+        // Prepares to shoot from the shooting position in NonAmpY
         NamedCommands.registerCommand("prepareNonAmpYShoot", CommandFactoryUtility.createPreparePosedShootEndlessCommand(turret, pivot, shooter, null, 
                 // y: 2.0 original value
                 // moved since it was too far to right  
@@ -183,12 +232,17 @@ public class AutoCommandManager {
                 // y: 2.0 original value
                 // moved since it was too far to right  
                 new Pose2d(3.98, 0.0, new Rotation2d(0.0)) ));
-
-        //This only affects the SpeakerScoreUtility calculated shot.
-        //THIS MUST BE RESET AFTER USE or it will mess up all future shots
+        
+        // Offsets future shots down by one degree
+        // THIS MUST BE RESET AFTER USE or it will mess up all future shots. In addition, this only affects the SpeakerScoreUtility calculated shot.
+        // ! - - - !THIS MUST BE RESET AFTER USE! - - - !
         NamedCommands.registerCommand("setShotOffset-Down1",  new InstantCommand(()->SpeakerScoreUtility.setShotOffset(-1.0)));
         NamedCommands.registerCommand("resetShotOffset",  new InstantCommand(()->SpeakerScoreUtility.resetShotOffset()));
 
+        // Sets the shooter's target speed to 100 rots/s instead of 117. 
+        // Used for slowing close-range shots so they don't bounce out of the speaker.
+        // THIS MUST BE RESET AFTER USE or it will mess up all future shots. In addition, this only affects the SpeakerScoreUtility calculated shot.
+        // ! - - - !THIS MUST BE RESET AFTER USE! - - - !
         NamedCommands.registerCommand("setShotSpeedOverride-100",  new InstantCommand(()->SpeakerScoreUtility.setShotSpeedOffset(100.0)));
         NamedCommands.registerCommand("resetShotSpeedOverride",  new InstantCommand(()->SpeakerScoreUtility.resetShotSpeedOffset()));
     }
