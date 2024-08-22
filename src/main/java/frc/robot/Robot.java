7b631f7bd346cc7e4f766bead5cc7e2bd6e13444
Jack Schumacher <84467112+jackrschumacher@users.noreply.github.com>
2024-08-22 18:49:43 -0500

    Set log file path using new advantagekit to fix hanging issue Possibly related to rio drive being full

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -44,8 +44,9 @@ public class Robot extends LoggedRobot {
 
     boolean replay = false;
     //Advantage Kit
+    // Logger.disableDeterministicTimestamps();
     if (isReal()) {
-        Logger.addDataReceiver(new WPILOGWriter()); // Log to a USB stick ("/U/logs")
+        Logger.addDataReceiver(new WPILOGWriter("/home/lvuser/logs")); // Log to a USB stick ("/U/logs")
         Logger.addDataReceiver(new NT4Publisher()); // Publish data to NetworkTables
         new PowerDistribution(1, ModuleType.kRev); // Enables power distribution logging
     } else {
